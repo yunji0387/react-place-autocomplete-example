61a365946addc1dc7b5f939d7fff22109d765368
yunji0387 <yunji0387@gmail.com>
2023-08-27 12:32:32 -0500

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import { useState, useMemo } from 'react';
+// import usePlacesAutocomplete, {
+//   getGeocode,
+//   getLatLng,
+// } from 'use-places-autocomplete';
+
+import React from 'react';
+import './App.css';
+import GoogleMaps from './GoogleMaps';
+
+
+function App() {
+  return (
+    <div>
+      <p>Hello World!</p>
+      <GoogleMaps />
+    </div>
+  );
+}
+
+export default App;
